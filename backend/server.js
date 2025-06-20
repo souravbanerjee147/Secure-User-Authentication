7166ddb70f2a58ece2a4c4f47ea98f74b77ca90c
souravbanerjee147 <163667731+souravbanerjee147@users.noreply.github.com>
2025-06-21 01:32:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
+const path = require('path');
 const authRoutes = require('./routes/auth.js');
 
 const app = express();
 const PORT = 5500;
+
+app.use(express.static(path.join(__dirname, '..')));
 
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
 }));
 
-// 🔽 These are the critical middleware
+
+
+
+// These are the critical middleware
 app.use(express.json());                    
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,6 +29,10 @@ app.use(session({
 
 // Routes
 app.use('/api', authRoutes);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
