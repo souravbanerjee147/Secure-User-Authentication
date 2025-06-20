419928d12be2897d96f4e29738625d25a7989b79
souravbanerjee147 <163667731+souravbanerjee147@users.noreply.github.com>
2025-06-20 23:46:16 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,16 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',      // use your MySQL username
+  password: 'Cockp!t147',      // your MySQL password
+  database: 'candidate'
+});
+
+db.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL Database');
+});
+
+module.exports = db;
 
